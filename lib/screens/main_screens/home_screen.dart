7e4b593c7ca2545bac8bb123bf28347c30ab91f5
AM <ammar260813@gmail.com>
2021-11-26 01:47:10 +0100

    Dashboard work

--- lib/screens/main_screens/home_screen.dart
+++ lib/screens/main_screens/home_screen.dart
@@ -1,26 +1,107 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen  extends StatefulWidget {
-  const HomeScreen ({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   _State createState() => _State();
 }
-class _State extends State<HomeScreen>{
+
+class _State extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Container(
+          color: Colors.blue,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height / 2,
+            child: ListView(
+              children: [
+                Container(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height / 50),
+                    child: const Center(
+                        child: Text(
+                      "Current income next month",
+                      style: TextStyle(color: Colors.white),
+                    ))),
+                Container(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height / 30),
+                  child: const Center(
+                      child: Text(
+                    "0 DKK",
+                    style: TextStyle(color: Colors.white, fontSize: 36),
+                  )),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CardFb2 extends StatelessWidget {
+  final String text;
+  final String imageUrl;
+  final String subtitle;
+  final Function() onPressed;
+
+  const CardFb2(
+      {required this.text,
+      required this.imageUrl,
+      required this.subtitle,
+      required this.onPressed,
+      Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height / 2),
-      color: Colors.blue,
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 7),
-              child: Text("Total income this month")
-          ),
-        ],
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 75,
+        padding: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.5),
+          boxShadow: [
+            BoxShadow(
+                offset: const Offset(10, 20),
+                blurRadius: 10,
+                spreadRadius: 0,
+                color: Colors.grey.withOpacity(.05)),
+          ],
+        ),
+        child: Row(
+          children: [
+            Image.network(imageUrl, height: 59, fit: BoxFit.cover),
+            const SizedBox(
+              width: 15,
+            ),
+            Text(text,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                )),
+            const Spacer(),
+            Text(
+              subtitle,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 12),
+            ),
+          ],
+        ),
       ),
     );
   }
